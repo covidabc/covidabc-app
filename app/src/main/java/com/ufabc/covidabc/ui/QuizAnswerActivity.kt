1ff90e7e60f32c5079575706ea95177ddb524b41
Kanan <kcs.kanan@gmail.com>
2020-06-26 21:14:11 -0300

    Estrutura inicial do quiz

---
+++ app/src/main/java/com/ufabc/covidabc/ui/QuizAnswerActivity.kt
@@ -0,0 +1,19 @@
+package com.ufabc.covidabc.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ufabc.covidabc.R
+
+class QuizAnswerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quiz_answer)
+
+    }
+
+
+
+
+}
+
---
+++ app/src/main/java/com/ufabc/covidabc/ui/QuizQuestionActivity.kt
@@ -0,0 +1,32 @@
+package com.ufabc.covidabc.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ufabc.covidabc.R
+import kotlinx.android.synthetic.main.quiz_question.*
+
+class QuizQuestionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quiz_question)
+
+        yesButton.setOnClickListener {
+            val intent = Intent(it.context, QuizAnswerActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+
+        noButton.setOnClickListener {
+            val intent = Intent(it.context, QuizAnswerActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+    }
+
+
+
+
+}
+
