26dcf5ab2fb78c9f62520de29ded48b44c271a27
Kanan <kcs.kanan@gmail.com>
2020-06-27 08:40:47 -0300

    Atualizacao no arquivo QuizFragment

--- app/src/main/java/com/ufabc/covidabc/ui/main/categories/QuizFragment.kt
+++ app/src/main/java/com/ufabc/covidabc/ui/main/categories/QuizFragment.kt
@@ -1,5 +1,6 @@
 package com.ufabc.covidabc.ui.main.categories
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ufabc.covidabc.R
+import com.ufabc.covidabc.ui.QuizQuestionActivity
+import kotlinx.android.synthetic.main.fragment_quiz.*
 
 class QuizFragment : Fragment() {
 
@@ -20,5 +23,13 @@ class QuizFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        quiz_enterButton.setOnClickListener {
+            val intent = Intent(it.context, QuizQuestionActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
+
+
+
 }
