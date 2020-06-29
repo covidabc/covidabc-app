19139b96e4af497133915fa75f127c836023ee8c
igornerest <igornerest@gmail.com>
2020-06-29 20:14:25 -0300

    Adicionando Spinner para selecao de tipo de evento

--- app/src/main/java/com/ufabc/covidabc/mainScreen/categories/Event/CreateEventActivity.kt
+++ app/src/main/java/com/ufabc/covidabc/mainScreen/categories/Event/CreateEventActivity.kt
@@ -2,19 +2,22 @@ package com.ufabc.covidabc.mainScreen.categories.Event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import com.ufabc.covidabc.R
 import com.ufabc.covidabc.model.CalendarEvent
 import com.ufabc.covidabc.model.CalendarEventDAO
 import java.util.*
 
 class CreateEventActivity : AppCompatActivity() {
-    private lateinit var eventNameEditText : EditText
+    private lateinit var eventTitleEditText : EditText
     private lateinit var eventPlaceEditText : EditText
     private lateinit var eventTypeEditText : EditText
     private lateinit var eventDescriptionEditText : EditText
     private lateinit var createEventButton : Button
+    private lateinit var eventTypeSpinner : Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +28,22 @@ class CreateEventActivity : AppCompatActivity() {
     }
 
     private fun setViews() {
-        eventNameEditText = findViewById(R.id.event_name_edit_text)
+        eventTitleEditText = findViewById(R.id.event_title_edit_text)
         eventPlaceEditText = findViewById(R.id.event_place_edit_text)
         eventTypeEditText = findViewById(R.id.event_type_edit_text)
         eventDescriptionEditText = findViewById(R.id.event_description_edit_text)
         createEventButton = findViewById(R.id.create_event_button)
+        eventTypeSpinner = findViewById(R.id.event_type_spinner)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.event_type_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            eventTypeSpinner.adapter = adapter
+        }
     }
 
     private fun setListeners() {
@@ -41,7 +55,7 @@ class CreateEventActivity : AppCompatActivity() {
     private fun createEvent() {
         val eventDate = Date(System.currentTimeMillis())
 
-        val newEvent = CalendarEvent(eventNameEditText.text.toString(), eventDescriptionEditText.text.toString(), eventDate, eventPlaceEditText.text.toString())
+        val newEvent = CalendarEvent(eventTitleEditText.text.toString(), eventDescriptionEditText.text.toString(), eventDate, eventPlaceEditText.text.toString())
         CalendarEventDAO.addEvent(newEvent)
     }
 }
