1f70b655cee63232a4413535949c5a6366ccbbdc
igornerest <igornerest@gmail.com>
2020-06-30 23:01:42 -0300

    Adicionando DatePickerDialog no cadastramento de evento

--- app/src/main/java/com/ufabc/covidabc/mainScreen/categories/Event/CreateEventActivity.kt
+++ app/src/main/java/com/ufabc/covidabc/mainScreen/categories/Event/CreateEventActivity.kt
@@ -1,5 +1,7 @@
 package com.ufabc.covidabc.mainScreen.categories.Event
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,14 +9,17 @@ import android.widget.*
 import com.ufabc.covidabc.R
 import com.ufabc.covidabc.model.CalendarEvent
 import com.ufabc.covidabc.model.CalendarEventDAO
+import java.text.SimpleDateFormat
 import java.util.*
 
 class CreateEventActivity : AppCompatActivity() {
     private lateinit var eventTitleEditText : EditText
     private lateinit var eventPlaceEditText : EditText
     private lateinit var eventDescriptionEditText : EditText
-    private lateinit var createEventButton : Button
+    private lateinit var eventDateEditText : EditText
     private lateinit var eventTypeSpinner : Spinner
+    private lateinit var createEventButton : Button
+    private lateinit var eventDate : Date
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,7 @@ class CreateEventActivity : AppCompatActivity() {
         eventTitleEditText = findViewById(R.id.event_title_edit_text)
         eventPlaceEditText = findViewById(R.id.event_place_edit_text)
         eventDescriptionEditText = findViewById(R.id.event_description_edit_text)
+        eventDateEditText = findViewById(R.id.event_date_edit_text)
         createEventButton = findViewById(R.id.create_event_button)
         eventTypeSpinner = findViewById(R.id.event_type_spinner)
 
@@ -43,16 +49,31 @@ class CreateEventActivity : AppCompatActivity() {
         createEventButton.setOnClickListener {
             createEvent()
         }
+
+        eventDateEditText.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener{ _, year, month, day ->
+                eventDate = Date(year, month, day)
+                eventDateEditText.setText("$day/$month/$year")
+            }
+
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
     }
 
     private fun createEvent() {
-        val eventDate = Date(System.currentTimeMillis())
         val eventType = eventTypeSpinner.selectedItem as CalendarEvent.EventType
         val newEvent = CalendarEvent(
             eventTitleEditText.text.toString(),
             eventType,
             eventDescriptionEditText.text.toString(),
-            eventDate,
+            this.eventDate,
             eventPlaceEditText.text.toString()
         )
         CalendarEventDAO.addEvent(newEvent)
