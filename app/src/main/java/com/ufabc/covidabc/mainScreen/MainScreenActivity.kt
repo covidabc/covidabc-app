3e79dbf75191578d563ec33794b194eb27c3c8b9
igornerest <igornerest@gmail.com>
2020-07-08 21:48:22 -0300

    Toolbar é setada junto com navController

--- app/src/main/java/com/ufabc/covidabc/mainScreen/MainScreenActivity.kt
+++ app/src/main/java/com/ufabc/covidabc/mainScreen/MainScreenActivity.kt
@@ -3,14 +3,17 @@ package com.ufabc.covidabc.mainScreen
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ufabc.covidabc.R
 
 class MainScreenActivity : AppCompatActivity() {
 
+    private lateinit var mainToolbar : Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
@@ -31,7 +34,8 @@ class MainScreenActivity : AppCompatActivity() {
                 R.id.navigation_quiz
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        mainToolbar = findViewById(R.id.main_screen_toolbar)
+        setupWithNavController(mainToolbar, navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
