7a276119758caceb420fd5f2d413030dfac56783
igornerest <igornerest@gmail.com>
2020-08-16 16:10:15 -0300

    Botão para adicionar eventos no espaço do brigadista

--- app/src/main/java/com/ufabc/covidabc/login/MyAccountActivity.kt
+++ app/src/main/java/com/ufabc/covidabc/login/MyAccountActivity.kt
@@ -8,11 +8,13 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.ufabc.covidabc.R
 import com.ufabc.covidabc.features.InventoryManagementActivity
+import com.ufabc.covidabc.mainScreen.categories.event.CreateEditEventActivity
 
 class MyAccountActivity : AppCompatActivity() {
 
     private lateinit var logoutButton : Button
     private lateinit var inventoryManagementButton : Button
+    private lateinit var createEventButton : Button
 
     private val mAuth =  FirebaseAuth.getInstance()
 
@@ -27,11 +29,16 @@ class MyAccountActivity : AppCompatActivity() {
     private fun setViews() {
         inventoryManagementButton = findViewById(R.id.inventory_management_button)
         logoutButton = findViewById(R.id.logout_button)
+        createEventButton = findViewById(R.id.create_event_button)
     }
 
     private fun setListeners() {
         inventoryManagementButton.setOnClickListener {
             startActivity(Intent(this, InventoryManagementActivity::class.java))
+        }
+
+        createEventButton.setOnClickListener {
+            startActivity(Intent(this, CreateEditEventActivity::class.java))
         }
 
         logoutButton.setOnClickListener {
